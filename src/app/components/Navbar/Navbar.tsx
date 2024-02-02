4c9d676bbc80c832447aa9f305b7f224880aeeaa
victorzarzar <victorzarzar58@gmail.com>
2024-02-02 06:10:33 -0300

    correction in my-5 margtin top and margin bottom NavBar Mobile

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -56,9 +56,9 @@ const Navbar: React.FC<NavbarProps> = ({ theme, setTheme }) => {
           {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-0 transition-all duration-500 ease-in text-center ${open ? "top-12" : "top-[-490px]"} ${open ? "h-screen flex flex-col justify-center items-center" : ""} ${MobileMenuBgColor}`}>
+          className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-0 transition-all duration-500 ease-in text-center ${open ? "top-10" : "top-[-490px]"} ${open ? "h-screen flex flex-col justify-center items-center" : ""} ${MobileMenuBgColor}`}>
           {Links.map((link) => (
-            <li className="md:ml-8 text-center md:my-0 my-7 font-semibold" key={link.name}>
+            <li className="md:ml-8 text-center md:my-0 my-5 font-semibold" key={link.name}>
               <Link href={`/${link.link}`} passHref>
                 <div className={Text}>{link.name}</div>
               </Link>
