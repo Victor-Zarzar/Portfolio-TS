6d8aeccfd437c076ee217165240f4f40932b8b64
victorzarzar <victorzarzar58@gmail.com>
2024-03-30 17:36:10 -0300

    correction in button header, and correction in skills.

--- src/app/components/Skills/TechItem.tsx
+++ src/app/components/Skills/TechItem.tsx
@@ -7,7 +7,7 @@ interface TechProps {
 
 export const TechItem = ({ text, icon }: TechProps) => {
     return (
-        <li className="flex p-2 md:p-4 space-x-2">  
+        <li className="flex p-2 md:p-4 space-x-2 mx-6 md:mx-0">  
             <span>{icon({ className: "h-4 w-4 md:h-6 md:w-6 text-gray-400" })}</span>
             <span className="text-gray-400 text-xs md:text-lg">{text}</span>   
         </li>
